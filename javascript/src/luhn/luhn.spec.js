efc89f45eb8d3417081cb7a689803b51caa9ac59
Andy Wright <atwright147@gmail.com>
2018-06-12 22:37:50 +0100

    create passing tests

--- javascript/src/luhn/luhn.spec.js
+++ javascript/src/luhn/luhn.spec.js
@@ -3,11 +3,11 @@ import { expect } from 'chai';
 import luhnCheck from './luhn.js';
 
 describe('#luhnCheck', () => {
-    it('should return true for a real number', () => {
-        expect(luhnCheck(123)).to.equal(true);
+    it('should return a number for a real number', () => {
+        expect(luhnCheck('123')).to.equal(123);
     });
 
-    it('should return false for a non-number', () => {
-        expect(luhnCheck('string')).to.equal(false);
+    it('should return NaN for a non-number', () => {
+        expect(luhnCheck('string')).to.be.NaN;
     });
 });
